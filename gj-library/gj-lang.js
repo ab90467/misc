d0821e841df1aacd7bda57c494f7066b90e65376
J9 <ovreberg@gmail.com>
2018-03-07 22:48:26 +0100

    Create gj-lang.js

---
+++ gj-library/gj-lang.js
@@ -0,0 +1,14 @@
+(function(gj) {
+
+
+
+    var searchLangObjAndReturnValue = function(val) {
+        return val;
+    };
+
+
+    // public API
+    gj.lang = {
+        _: searchLangObjAndReturnValue
+    };
+})((window.gj = window.gj || {}));
